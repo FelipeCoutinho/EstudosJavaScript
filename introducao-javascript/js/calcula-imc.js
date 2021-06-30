8f805a6f56cb85ae8f12cd5ce77a3692a5b5a30c
Felipe Coutinho <philipecout@gmail.com>
2021-06-30 13:59:45 -0300

    busca paciente

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -5,48 +5,47 @@ titulo.textContent = 'Aparecida Nutricionista';
 const pacientes = document.querySelectorAll(".paciente");
 
 pacientes.forEach(paciente => {
-    
-    const peso =   paciente.querySelector('.info-peso');
+
+    const peso = paciente.querySelector('.info-peso');
     const altura = paciente.querySelector('.info-altura');
     const tdImc = paciente.querySelector(".info-imc");
-    const imc = calculaImc(peso,altura)
-    
+    const imc = calculaImc(peso, altura)
+
     let alturaEhValida = validaAltura(altura.textContent);
     let pesoEhValido = validaPeso(peso.textContent);
-    
-    if(!pesoEhValido){
+
+    if (!pesoEhValido) {
         paciente.classList.add("pacienteInvalido");
         tdImc.textContent = "O Peso informado é invalido";
         return;
     }
 
-    if(!alturaEhValida){
+    if (!alturaEhValida) {
         paciente.classList.add("pacienteInvalido");
         tdImc.textContent = "A Altura informada é invalida";
         return;
-    }   
+    }
 
     tdImc.textContent = imc
 })
 
-function calculaImc( peso, altura ) {
-   const imc  = Number(peso.textContent)/Math.pow(Number(altura.textContent),2);
-   return imc.toFixed(2);
+function calculaImc(peso, altura) {
+    const imc = Number(peso.textContent) / Math.pow(Number(altura.textContent), 2);
+    return imc.toFixed(2);
 }
 
-function validaPeso(peso){
-    console.log(peso);
-    if(peso >=0 && peso <=270){
+function validaPeso(peso) {
+    if (peso >= 0 && peso <= 270) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function validaAltura(altura){
-    if(altura >= 0 && altura <=5){
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 5) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -1,12 +1,12 @@
 
 const btnHendleForm = document.querySelector('#adicionarPaciente');
-btnHendleForm.addEventListener('click',function (Event) {
+btnHendleForm.addEventListener('click', function (Event) {
     Event.preventDefault();
 
     const dataForm = document.querySelector("#formAdiciona");
-    const paciente  = getDadosPaciente(dataForm);
+    const paciente = getDadosPaciente(dataForm);
 
-    const table =  document.querySelector("table")
+    const table = document.querySelector("table")
     const tr = montaTr(paciente)
 
     table.appendChild(tr)
@@ -15,13 +15,28 @@ btnHendleForm.addEventListener('click',function (Event) {
     removeMensagemError()
 });
 
-function getDadosPaciente(dataForm){
+const removoPacientes = document.querySelectorAll('table');
+
+removoPacientes.forEach(paciente => {
+    paciente.addEventListener("dblclick", function (Event) {
+        let paiAlvo = Event.target.parentNode; // TR -que sera removida 
+        paiAlvo.classList.add("fadeOut")
+
+        setTimeout(() => {
+            paiAlvo.remove();
+        }, 500)
+
+    })
+})
+
+
+function getDadosPaciente(dataForm) {
     paciente = {
         nome: dataForm.nome.value,
         peso: dataForm.peso.value,
         altura: dataForm.altura.value,
         gordura: dataForm.gordura.value,
-        imc:calculaImc(dataForm.peso.value,dataForm.altura.value)
+        imc: calculaImc(dataForm.peso.value, dataForm.altura.value)
     }
     return paciente;
 }
@@ -30,21 +45,21 @@ function montaTr(paciente) {
     const tr = document.createElement("tr");
     const errors = pacienteEhValido(paciente)
 
-    if(errors.length > 0){
+    if (errors.length > 0) {
         console.log('paciente invalido');
         return;
     }
-    
-    tr.appendChild(montaTd(paciente.nome,"info-nome"));
-    tr.appendChild(montaTd(paciente.peso,"info-peso"));
-    tr.appendChild(montaTd(paciente.altura,"info-altura"));
-    tr.appendChild(montaTd(paciente.gordura,"info-gordura"))
-    tr.appendChild(montaTd(paciente.imc,"info-imc"));
+
+    tr.appendChild(montaTd(paciente.nome, "info-nome"));
+    tr.appendChild(montaTd(paciente.peso, "info-peso"));
+    tr.appendChild(montaTd(paciente.altura, "info-altura"));
+    tr.appendChild(montaTd(paciente.gordura, "info-gordura"))
+    tr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return tr;
 }
 
-function montaTd(data,Class){
+function montaTd(data, Class) {
     const td = document.createElement("td");
     td.textContent = data;
     td.classList.add(Class);
@@ -52,21 +67,21 @@ function montaTd(data,Class){
 
 }
 
-function pacienteEhValido(paciente){
+function pacienteEhValido(paciente) {
     let error = []
-    
-    if(paciente.peso == "" || paciente.altura == ""){
+
+    if (paciente.peso == "" || paciente.altura == "") {
         error.push("Informe os campos peso e altura")
         exibeMensagemError(error)
         return fal;
     }
-    
-    if(!validaPeso(paciente.peso)){
+
+    if (!validaPeso(paciente.peso)) {
         error.push('o peso informado é invalido');
         exibeMensagemError(error)
     }
-    
-    if(!validaAltura(paciente.altura)){
+
+    if (!validaAltura(paciente.altura)) {
         error.push('a altura informada é invalida')
         exibeMensagemError(error)
     }
@@ -74,11 +89,11 @@ function pacienteEhValido(paciente){
 
 }
 
-function exibeMensagemError(msg){
+function exibeMensagemError(msg) {
     const ul = document.querySelector('#msg-errors')
     ul.innerHTML = ""; // controla o html interno do elelmento
-    if(msg.length >0){
-        msg.forEach((msg,i) => {
+    if (msg.length > 0) {
+        msg.forEach((msg, i) => {
             let li = document.createElement('li')
             li.textContent = msg
             li.classList.add('msgPacienteInvalido')
@@ -87,7 +102,31 @@ function exibeMensagemError(msg){
     }
 
 }
-function removeMensagemError(){
+function removeMensagemError() {
     const ul = document.querySelector('#msg-errors')
     ul.innerHTML = ""; // controla o html interno do elelmento
 }
+
+//Busca de Paciente
+const filter = document.querySelector("#filtrarTabela");
+filter.addEventListener("input", function () {
+
+    const pacientes = document.querySelectorAll(".paciente");
+    if (this.value.length > 0) {
+        pacientes.forEach(paciente => {
+
+            const pacienteFiltrado = paciente.querySelector(".info-nome");
+
+            if (filter.value != pacienteFiltrado.textContent) {
+                paciente.classList.add('invisivel');
+            } else {
+                paciente.classList.remove('invisivel');
+            }
+        })
+    } else {
+        pacientes.forEach(paciente => {
+            console.log(paciente);
+            paciente.classList.remove('invisivel')
+        })
+    }
+})
